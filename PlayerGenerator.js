fc1426b87f47c8e05d46102ffe35d7a61740343c
andy herbert <andy.herbert1875@gmail.com>
2024-09-16 15:30:57 +0100

    Testing

--- PlayerGenerator.js
+++ PlayerGenerator.js
@@ -1,3 +1,6 @@
+// ---- TESTING
+
+
 const player = ['Sam Smith', 'Kelvin E', 'Lewis Wing', 'Tyler Bindon', 'Harvey Knibbs', 'Joel Perreira', 'Andy Yiadom', 'Femi Azeez', 'Amadou Mbengue', 'Jeriel Dorsett'];
 const goals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
 const newClub = ['Birmingham', 'Millwall', 'Madrid', 'Barca', 'Salzburg', 'City', 'United', 'Laurel Park']
